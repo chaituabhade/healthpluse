287b7fad8dfe7f2d7126569df894ee23fd3079d1
chaituabhade <156505752+chaituabhade@users.noreply.github.com>
2024-07-29 14:47:24 +0530

    Add files via upload

---
+++ app/how_to_use/page.tsx
@@ -0,0 +1,312 @@
+"use client"
+import Image from "next/image";
+// import Header from "./_components/Header";
+//import Hero from "./_components/Hero";
+// import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
+import { useEffect } from "react";
+import MaxWidthWrapper from '@/components/MaxWidthWrapper'
+import Footer from '@/components/Footer'
+import Link from 'next/link'
+import { ArrowRight } from 'lucide-react'
+import { buttonVariants } from '@/components/ui/button'
+
+
+export default function Home() {
+
+  
+
+  return (
+    <>
+      
+      <MaxWidthWrapper className='mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center'>
+        <div className='mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50'>
+          <p className='text-sm font-semibold text-gray-700'>
+             Now <span className='text-green-500'>CarePluse</span> is live
+          </p>
+        </div>
+        <h1 className='max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl'>
+          <span className='text-green-500'>CarePluse,</span>{' '}
+          Instant appointment at your footstep.
+        </h1>
+        <p className='mt-5 max-w-prose text-zinc-700 sm:text-lg'>
+          Eraser allows you to bring your ideas to life on a digital canvas. Simply start creating, and watch your imagination take shape instantly.
+        </p>
+
+        
+      </MaxWidthWrapper>
+
+      
+      
+      {/* value proposition section */}
+      <div>
+        <div className='relative isolate'>
+          <div
+            aria-hidden='true'
+            className='pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80'>
+            <div
+              style={{
+                clipPath:
+                  'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+              }}
+              className='relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]'
+            />
+          </div>
+
+         
+
+          <div
+            aria-hidden='true'
+            className='pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80'>
+            <div
+              style={{
+                clipPath:
+                  'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+              }}
+              className='relative left-[calc(50%-13rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-36rem)] sm:w-[72.1875rem]'
+            />
+          </div>
+        </div>
+      </div>
+
+      {/* Feature section */}
+      <div className='mx-auto mb-32 mt-32 max-w-5xl sm:mt-56'>
+        <div className='mb-12 px-6 lg:px-8'>
+          <div className='mx-auto max-w-2xl sm:text-center'>
+            <h2 className='mt-2 font-bold text-4xl text-white sm:text-5xl'>
+              Starting Steps
+            </h2>
+            <p className='mt-4 text-lg text-gray-600'>
+              Bringing your ideas to life has never been easier than with Eraser. Start creating on your digital canvas in minutes.
+            </p>
+          </div>
+        </div>
+
+         
+        <ol className='my-8 space-y-4 pt-8 md:flex md:space-x-12 md:space-y-0'>
+          <li className='md:flex-1'>
+            <div className='flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4'>
+              <span className='text-sm font-medium text-green-500'>
+                Step 1
+              </span>
+              <span className='text-xl font-semibold'>
+                Sign up for an account
+              </span>
+              <span className='mt-2 text-zinc-700'>
+                Create a new account with your email address ans phone number
+              </span>
+            </div>
+          </li>
+          <li className='md:flex-1'>
+            <div className='flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4'>
+              <span className='text-sm font-medium text-green-500'>
+                Step 2
+              </span>
+              <span className='text-xl font-semibold'>
+                Fill medical information
+              </span>
+              <span className='mt-2 text-zinc-700'>
+                Give your past and family Medical History information and identity 
+              </span>
+            </div>
+          </li>
+          <li className='md:flex-1'>
+            <div className='flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4'>
+              <span className='text-sm font-medium text-green-500'>
+                Step 3
+              </span>
+              <span className='text-xl font-semibold'>
+                Schedule appointment 
+              </span>
+              <span className='mt-2 text-zinc-700'>
+              Schedule the appointment and select the physician
+              </span>
+            </div>
+          </li>
+          <li className='md:flex-1'>
+            <div className='flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4'>
+              <span className='text-sm font-medium text-green-500'>
+                Step 4
+              </span>
+              <span className='text-xl font-semibold'>
+                Get conformed
+              </span>
+              <span className='mt-2 text-zinc-700'>
+                Respective physician will comform yor appointment and you will get notifications via sms
+              </span>
+            </div>
+          </li>
+        </ol> 
+
+        <div>
+            <div className='mx-auto max-w-6xl px-6 lg:px-8'>
+              <div className='mt-16 flow-root sm:mt-24'>
+                <div className='-m-2 rounded-xl bg-zinc-400 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4'>
+                  <Image
+                    src='/assets/images/home.PNG'
+                    alt='product preview'
+                    width={1364}
+                    height={866}
+                    quality={100}
+                    className='rounded-md bg-zinc-900 p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10'
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+
+        <div className='mx-auto max-w-6xl px-6 lg:px-8'>
+          <div className='mt-16 flow-root sm:mt-24'>
+            <div className='-m-2 rounded-xl bg-zinc-400 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4'>
+              <Image
+                src='/assets/images/home4.PNG'
+                alt='uploading preview'
+                width={1419}
+                height={732}
+                quality={100}
+                className='rounded-md bg-zinc-900 p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10'
+              />
+            </div>
+          </div>
+        </div>
+
+        {/* value proposition section */}
+      <div>
+        <div className='relative isolate'>
+          <div
+            aria-hidden='true'
+            className='pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80'>
+            <div
+              style={{
+                clipPath:
+                  'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+              }}
+              className='relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]'
+            />
+          </div>
+
+         
+
+          <div
+            aria-hidden='true'
+            className='pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80'>
+            <div
+              style={{
+                clipPath:
+                  'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+              }}
+              className='relative left-[calc(50%-13rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-36rem)] sm:w-[72.1875rem]'
+            />
+          </div>
+        </div>
+      </div>
+
+        
+
+        <div>
+          
+          <div className='mx-auto max-w-9xl px-6  lg:px-8'>
+            <div className='mt-16 flow-root sm:mt-24'>
+              <div className='-m-2 rounded-xl bg-zinc-400 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4'>
+                <Image
+                  src='/assets/images/home2.PNG'
+                  alt='product preview'
+                  width={1500}
+                  height={1000}
+                  quality={100}
+                  className='rounded-md bg-zinc-900 p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10'
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className='mx-auto max-w-9xl px-6  lg:px-8'>
+            <div className='mt-16 flow-root sm:mt-24'>
+              <div className='-m-2 rounded-xl bg-zinc-400 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4'>
+                <Image
+                  src='/assets/images/home3.PNG'
+                  alt='product preview'
+                  width={1500}
+                  height={1000}
+                  quality={100}
+                  className='rounded-md bg-zinc-900 p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10'
+                />
+              </div>
+            </div>
+          </div>
+          
+         
+
+          <div className="flex">
+          <div className='mx-auto max-w-9xl px-6  lg:px-8'>
+            <div className='mt-16 flow-root sm:mt-24'>
+              <div className='-m-2 rounded-xl bg-zinc-400 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4'>
+                <Image
+                  src='/assets/images/home6.jpg'
+                  alt='product preview'
+                  width={400}
+                  height={700}
+                  quality={100}
+                  className='rounded-md bg-zinc-900 p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10'
+                />
+              </div>
+            </div>
+          </div>
+          <div className='mx-auto max-w-9xl px-6  lg:px-8'>
+            <div className='mt-16 flow-root sm:mt-24'>
+              <div className='-m-2 rounded-xl bg-zinc-400 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4'>
+                <Image
+                  src='/assets/images/home7.jpg'
+                  alt='product preview'
+                  width={400}
+                  height={700}
+                  quality={100}
+                  className='rounded-md bg-zinc-900 p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10'
+                />
+              </div>
+            </div>
+          </div>
+          </div>
+
+           {/* value proposition section */}
+      <div>
+        <div className='relative isolate'>
+          <div
+            aria-hidden='true'
+            className='pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80'>
+            <div
+              style={{
+                clipPath:
+                  'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+              }}
+              className='relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]'
+            />
+          </div>
+
+         
+
+          <div
+            aria-hidden='true'
+            className='pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80'>
+            <div
+              style={{
+                clipPath:
+                  'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+              }}
+              className='relative left-[calc(50%-13rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-36rem)] sm:w-[72.1875rem]'
+            />
+          </div>
+        </div>
+      </div>
+            
+          <p className=' px-60  mt-8 text-gray-600'>
+                Made by Chaitanya Abhade visit github for more info  <span className="text-blue-400"><a href="https://github.com/chaituabhade" >GITHUB</a></span>
+          </p>
+        </div>
+
+      </div>
+      
+      {/* <Footer/> */}
+    </>
+  )
+
+}
