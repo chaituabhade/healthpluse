4171e42a6b39463c4c3345f5b9d06d0b6c8d6b0d
chaituabhade <156505752+chaituabhade@users.noreply.github.com>
2024-07-29 20:52:31 +0530

    Update index.ts

--- constants/index.ts
+++ constants/index.ts
@@ -44,36 +44,45 @@ export const Doctors = [
   {
     image: "/assets/images/dr-green.png",
     name: "Nishikant Chavan",
+    Description: "(Dentist) MBBS with 10 year's experience"
   },
   {
     image: "/assets/images/dr-cameron.png",
     name: "ram kale",
+    Description: "(Nurologist) MBBS with 17 year's experience"
   },
   {
     image: "/assets/images/dr-livingston.png",
     name: "faraz khan",
+    Description: "(Surgeon) MD with 17 year's experience"
   },
   {
     image: "/assets/images/dr-peter.png",
     name: "Narayan Murti",
+    Description: "(Neurosurgeons) MBBS with 13 year's experience"
   },
   {
     image: "/assets/images/dr-remirez.png",
     name: "Amit Kale",
+    Description: "(Orthopedic Surgeons) MD with 10 year's experience"
   },
   {
     image: "/assets/images/dr-lee.png",
     name: "kunal Rao",
+    Description: "(Cardiologists) MD with 20 year's experience"
   },
   {
     image: "/assets/images/dr-cruz.png",
     name: "Mohan Bhagvat",
+    Description: "(Gastroenterologists) MBBS with 10 year's experience"
   },
   {
     image: "/assets/images/dr-sharma.png",
     name: "Hardik Sharma",
+    Description: "(Pulmonologists) MBBS with 6 year's experience"
   },
 ];
+
 
 export const StatusIcon = {
   scheduled: "/assets/icons/check.svg",
